cf008013869f1e7cc4427c9fcf538cc98a6a6a13
maaurissio <maur.gajardo@duocuc.cl>
2024-06-19 11:12:37 -0400

    super programa

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+
+while True:
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+    opcion = input("Elija una opción: ")
+    match opcion:
+        case "1": #Suma
+            pass
+        case "2": #Resta
+            pass
+        case "3": #Multiplicación
+            pass
+        case "4": #División
+            pass
+        case "5": #Salir
+            break
