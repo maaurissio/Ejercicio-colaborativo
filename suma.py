b559dbc508e34cc7420b7ed8c51a1003be63c432
maaurissio <maur.gajardo@duocuc.cl>
2024-06-19 11:21:19 -0400

    agregado fun suma

---
+++ suma.py
@@ -0,0 +1,6 @@
+def suma():
+    num1 = int(input("Introduce el primer número: "))
+    num2 = int(input("Introduce el segundo número: "))
+    print(f"El resultado de la suma es: {num1 + num2}")
+    input("Presiona una tecla para continuar...")
+    return
